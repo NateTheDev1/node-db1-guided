de77b5aff5bcc06263b5494b966ab335f4e83c8a
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-13 13:20:48 -0400

    get by id and get all

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,41 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  db.select("*")
+    .from("posts")
+    .then((posts) => {
+      res.status(200).json({ data: posts });
+    })
+    .catch((err) => {
+      handleError();
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  db.select("*")
+    .from("posts")
+    .where({ id: req.params.id })
+    .then((post) => {
+      res.status(200).json({ data: post });
+    })
+    .catch((err) => {
+      handleError();
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.put("/:id", (req, res) => {});
 
-router.put('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+function handleError(err) {
+  res.status(500).json({ message: error.message });
+}
 
 module.exports = router;
